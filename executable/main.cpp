489ce0c03c42497958f06d9c50e34aeeaf122171
Calasans <127008180+ocalasans@users.noreply.github.com>
2025-11-02 18:00:45 -0300

    Correction of warning, and "error RC2135: file not found: icons g.ico".

--- executable/main.cpp
+++ executable/main.cpp
@@ -53,7 +53,7 @@ namespace {
 }
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
-    int argc;
+    int argc = 0;
     std::unique_ptr<LPWSTR[], decltype(&LocalFree)> argv(CommandLineToArgvW(GetCommandLineW(), &argc), &LocalFree);
 
     if (!argv)
